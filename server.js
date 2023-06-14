62e1824f641ee7fa9a5624093815759d0a7e170c
Abdul Hussain <a.r.hussain@hotmail.com>
2023-06-14 01:49:16 +0100

    Set up our server

---
+++ server.js
@@ -0,0 +1,25 @@
+import express, { json } from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = 3000;
+
+app.use(json());
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
+
+// connect to a local mongo db
+mongoose
+	.connect("mongodb://localhost:27017/mydatabase", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("Connected to MongoDB"))
+	.catch((err) => console.error("Failed to connect to MongoDB", err));
+
+// Create our first route
+app.get("/", (req, res) => {
+	res.send("Hello HOPE");
+});
