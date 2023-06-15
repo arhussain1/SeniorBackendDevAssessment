cb02da874967b3b518c638d6e994abf2f84612da
Abdul Hussain <a.r.hussain@hotmail.com>
2023-06-15 23:59:10 +0100

    Add logic to suspend single student

---
+++ controllers/suspendStudent.js
@@ -0,0 +1,28 @@
+import Student from "../models/student.js";
+
+const SuspendStudentController = {
+	suspendStudent: async (req, res) => {
+		// This endpoint will need to suspend a single student
+		// it accepts a json object of "student" it is their email
+		//
+		// From initial looks there is no way to verify the request
+		// I would have liked a teacher email to be passed through as well
+		// then run a look up via registrations and verify the teacher is related
+		// to the student then process the suspension request
+
+		const { student: studentEmail } = req.body;
+
+		try {
+			await Student.findOneAndUpdate(
+				{ email: studentEmail },
+				{ isSuspended: true }
+			);
+
+			res.sendStatus(204);
+		} catch (error) {
+			res.status(500).json({ message: error.message });
+		}
+	},
+};
+
+export default SuspendStudentController;
